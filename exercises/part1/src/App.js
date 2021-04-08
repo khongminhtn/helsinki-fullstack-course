61c3def540c077e1bf0a8204ee7f77dca3767bbe
khongminhtn <tuyenminhkhong@live.co.uk>
2021-04-08 15:45:04 +0100

    finished part B

--- exercises/part1/src/App.js
+++ exercises/part1/src/App.js
@@ -14,42 +14,46 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercise}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercise}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercise}/>
     </>
   )
 }
 
 const Total = (props) => {
+  const total = props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise
   return (
     <>
-      <p>Number of exercises {props.totalExercises}</p>
+      <p>Number of exercises {total}</p>
     </>
   )
 }
 
 
 const App = () => {
-  const course = 'Half stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercise: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercise: 7
+      },
+      {
+        name: "State of a component",
+        exercise: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header title={course}/>
-      <Content 
-        part1={part1}
-        part2={part2}
-        part3={part3} 
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total totalExercises={exercises1 + exercises2 + exercises3}/>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
--- notes/part1.md
+++ notes/part1.md
@@ -1,8 +1,35 @@
 # React
-### Notes
+### React Notes
 1. Uses ES6 
 2. JSX
     - under the hood it is compiles to React.createElement()
     - it is compiled by Babel
     - XML-like
 3. Core philosophy of react is composing applications from many specialized reusable components
+### JavaScript Notes
+- Official name of JavaScript standard is ECMAScript
+- Latest release is ECMAScript2020 or ES11
+- Most transpiles done with Babel
+- Node.js is a JavaScript runtime environment
+- Variables
+    - const defines a value that cannot be changed
+    - let defines a normal variable
+    - ill-advised to use var as a way of defining variables
+- Array
+    - list.push(items) adds items to list
+    - list.forEach(value => {expressions}) iterates through the list
+    - Functional programming is often used in React
+        - Use of immutable data structures
+        - In React, it is preferable to use concat method
+            - Creates a new array rather than adding to existing array 
+            - list.concat(item)
+        - rather than forEach, list.map(value => {expressions}) is used
+            - Creates a new list.
+        - Destructuring Assisngment
+            - t = [1,2,3,4,5]
+            - [item1, item2, ..] = t
+            - item1 => 1
+- Objects
+    - JS common method of defining objects
+        - Object Literals -> similar to Python dictionary
+    - JS does not have classes in the same sense as Object Oriented Programming
