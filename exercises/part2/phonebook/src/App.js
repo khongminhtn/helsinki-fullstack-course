c000ca660f01567d304a5f4768717fbc202415e4
khongminhtn <tuyenminhkhong@live.co.uk>
2021-05-04 15:13:11 +0100

    completed exercise 2.8

--- exercises/part2/phonebook/src/App.js
+++ exercises/part2/phonebook/src/App.js
@@ -2,17 +2,24 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: ''
+    }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNum, setNewNum] = useState('')
 
 
   const updateNewName = (e) => setNewName(e.target.value)
-  const addName = (e) => {
+  const updateNewNum = (e) => setNewNum(e.target.value)
+  const addPerson = (e) => {
     e.preventDefault()
-    // concat metho merges 2 arrays and generate a new compeleted array
+    // concat method merges 2 arrays and generate a new compeleted array
     // then replace persons with the newly merged array
-    const newNameObject = [{ name: newName }]
+    const newNameObject = [{ 
+      name: newName,
+      number: newNum 
+    }]
     persons.find(person => person.name === newName) === undefined
     ? setPersons(persons.concat(newNameObject))
     : window.alert(`${newName} is already added to phonebook`)
@@ -22,17 +29,16 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
+        <div>name: <input onChange={updateNewName}/></div>
+        <div>number: <input onChange={updateNewNum}/></div>
         <div>
-          name: <input onChange={updateNewName}/>
-        </div>
-        <div>
-          <button onClick={addName} type="submit">add</button>
+          <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map(person => <li key={person.name}>{person.name}</li>)
+          persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
         }
       </ul>
     </div>
