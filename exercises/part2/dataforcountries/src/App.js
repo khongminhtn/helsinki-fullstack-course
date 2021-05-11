a6206334a66eb49bf4b0105c9ac8b80eb41777e0
khongminhtn <tuyenminhkhong@live.co.uk>
2021-05-11 14:01:54 +0100

    completed exercise 2.14

--- exercises/part2/dataforcountries/src/App.js
+++ exercises/part2/dataforcountries/src/App.js
@@ -16,7 +16,7 @@ const CountryDetail = ({country}) => {
   const [showCountry, setShowCountry] = useState(false)
   const toggleShowCountry = () => setShowCountry(!showCountry)
   return (
-    <div key={country.namw}>
+    <div>
       <>{country.name}</>
       <button onClick={toggleShowCountry}>show</button>
       {
@@ -31,7 +31,7 @@ const CountryDetail = ({country}) => {
                 country.languages.map(language => <li key={language.name}>{language.name}</li>)
               }
             </ul>
-            <img src={country.flag} width='150' height='100'/>
+            <img src={country.flag} width='150' height='100' alt='flag'/>
           </>
         : null
       }
@@ -40,16 +40,32 @@ const CountryDetail = ({country}) => {
 }
 
 const DisplayCountries = (props) => {
-  // Displays filtered countries
-  const countriesToDisplay = props.countries.filter(country => country.name.toLowerCase().includes(props.searchInput.toLowerCase()))
+  // Filtered Countries
+  const countriesToDisplay = props.countriesToDisplay
+
+  // Weather request params
+  const [weather, setWeather] = useState()
+  useEffect(() => {
+    const endPoint = 'https://api.weatherstack.com/current'
+    const params = {
+      access_key: process.env.REACT_APP_API_KEY,
+      query: countriesToDisplay.length === 1 ? countriesToDisplay[0].name : null
+    }
+    if (countriesToDisplay.length === 1) {
+      console.log('Fetching Weather...') ////
+      axios
+        .get(endPoint, {params})
+        .then(response => setWeather(response.data.current))
+    }
+  }, [countriesToDisplay])
+
+
   return(
     <div>
       {
-        props.searchInput.length === 0
-        ? <div/>
-        : countriesToDisplay.length > 10
+        countriesToDisplay.length > 10
         ? <>Too many matches, specify another filter</>
-        : countriesToDisplay.length === 1
+        : countriesToDisplay.length === 1 && weather
         ? <>
             <h1>{countriesToDisplay[0].name}</h1>
             <div>Capital: {countriesToDisplay[0].capital}</div>
@@ -60,39 +76,45 @@ const DisplayCountries = (props) => {
                 countriesToDisplay[0].languages.map(language => <li key={language.name}>{language.name}</li>)
               }
             </ul>
-            <img src={countriesToDisplay[0].flag} width='150' height='100'/>
+            <img src={countriesToDisplay[0].flag} width='150' height='100'alt='flag'/>
+            <h3>Weather in {countriesToDisplay[0].name}</h3>
+            <h6>temperature: {weather.temperature}</h6>
+            <img src={weather.weather_icons} width='100' height='100' alt='weather'/>
+            <h6>wind: {weather.wind_speed} mph direction {weather.wind_dir}</h6>
           </>
-        : countriesToDisplay.map(country => <CountryDetail country={country}/>)
+        : countriesToDisplay.map(country => <CountryDetail key={country.name} country={country}/>)
       }
     </div>
   )
 }
 
 const App = () => {
+
   // Fetches countries data
   const [countries, setCountries] = useState([])
   useEffect(() => {
-    console.log("Fetching countries...") /////////////////
+    console.log('Fetching Countries..') ////
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('Promise fulfilled') /////////////////
-        console.log(response) ////////////////////////////
         // response.data list of objects, contain countries info
         setCountries(response.data)
       })
   },[])
 
-  // Find countries
+  // Find and filter countries
   const [searchInput, setSearchInput] = useState('')
   const searchCountry = (e) => setSearchInput(e.target.value)
-
+  const countriesToDisplay = countries.filter(country => country.name.toLowerCase().includes(searchInput.toLowerCase()))
   return (
     <div className="App">
-      <Search searchCountry={searchCountry}/>
-      <DisplayCountries 
-        countries={countries}
-        searchInput={searchInput}/>
+      <Search 
+        searchCountry={searchCountry}/>
+      {
+        searchInput.length === 0
+        ? <div/>
+        : <DisplayCountries countriesToDisplay={countriesToDisplay}/>
+      }
     </div>
   )
 }
